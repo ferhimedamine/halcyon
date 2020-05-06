89f24b31ac458cb4fe0f06fd37b7aa24ffd9235e
Chris <cpoulter@hotmail.co.uk>
2020-05-06 10:20:26 +0100

    code clean up

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,8 +1,6 @@
-const config = {
+export default {
     GRAPHQL_URL: process.env.REACT_APP_GRAPHQL_URL || '/api',
     GRAPHQL_WS: process.env.REACT_APP_GRAPHQL_WS || 'ws://localhost:3000/api',
     SENTRY_DSN: process.env.REACT_APP_SENTRY_DSN,
     SENTRY_ENVIRONMENT: process.env.REACT_APP_SENTRY_ENVIRONMENT
 };
-
-export default config;

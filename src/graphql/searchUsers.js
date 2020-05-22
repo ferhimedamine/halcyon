c6df41927bf80c83d705db1e1dd262adb77d755b
Chris <cpoulter@hotmail.co.uk>
2020-05-22 08:56:10 +0100

    graphql query refactor

--- src/graphql/searchUsers.js
+++ src/graphql/searchUsers.js
@@ -1,19 +1,5 @@
 import { gql } from 'apollo-boost';
 
-export const GET_PROFILE = gql`
-    query GetProfile {
-        getProfile {
-            id
-            emailAddress
-            firstName
-            lastName
-            dateOfBirth
-            isLockedOut
-            roles
-        }
-    }
-`;
-
 export const SEARCH_USERS = gql`
     query SearchUsers(
         $size: Int
@@ -40,20 +26,6 @@ export const SEARCH_USERS = gql`
             }
             before
             after
-        }
-    }
-`;
-
-export const GET_USER_BY_ID = gql`
-    query GetUserById($id: ID!) {
-        getUserById(id: $id) {
-            id
-            emailAddress
-            firstName
-            lastName
-            dateOfBirth
-            isLockedOut
-            roles
         }
     }
 `;
